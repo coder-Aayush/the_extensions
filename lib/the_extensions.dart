87649403de92f1de167b1ea0281c53abacffce1f
coder-Aayush <bhattaraiaayush08@gmail.com>
2023-06-01 11:08:51 +0545

    Bumped: base class and file

--- lib/the_extensions.dart
+++ lib/the_extensions.dart
@@ -1,4 +1,3 @@
 library the_extensions;
 
 export 'src/list_extension.dart';
-export 'src/the_extensions_base.dart';
--- test/the_extensions_test.dart
+++ test/the_extensions_test.dart
@@ -1,16 +1,7 @@
-import 'package:the_extensions/the_extensions.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  group('List Extension', () {
+    
   });
 }
